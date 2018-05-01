25d7566ca598be8dc197d4a68ddc5b8b6b33d6d8
Evan <ejo1188@rit.edu>
2018-05-01 13:06:13 -0400

    Added material UI/SASS
    Added the material UI components and the sass loaders to apply styles as needed

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import style from './appStyle.scss';
+
 
 class Home extends React.Component {
 
+
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
+		const { classes } = this.props;
+
+		var titleStyle = {
+				flex: 1
+			};
 		return (
-				<h1>Hello world</h1>
+			<div>
+		      <AppBar className="test" position="static">
+		        <Toolbar>
+		          <IconButton color="inherit" aria-label="Menu">
+		            <MenuIcon />
+		          </IconButton>
+		          <Typography variant="title" style={titleStyle} color="inherit">
+		            Title
+		          </Typography>
+		          <Button color="inherit">Login</Button>
+		        </Toolbar>
+		      </AppBar>
+		    </div>
 			);
 	}		
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,16 @@ module.exports = {
 	          loader: 'babel-loader'
 	        },
 	     },
-	        // test: /\.scss$/,
-	        // use: {
-	        //   loader: 'style!css!resolve-url!sass?sourceMap'
-	        // }
-	      //}
+	     {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+	      }
 	    ]
-	  },
-// 	devtool: 'sourcemap',
+	  }
  };
