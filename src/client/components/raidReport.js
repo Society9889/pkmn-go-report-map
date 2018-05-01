87d358aea7c48586106951e98b6d654c3f1cefd0
Evan <ejo1188@rit.edu>
2018-05-01 16:18:40 -0400

    Raid report grid added

--- src/client/components/raidReport.js
+++ src/client/components/raidReport.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
 
 //style
 import style from './appStyle.scss';
@@ -14,7 +16,39 @@ class RaidReport extends React.Component {
 	render() {
 	
 		return (
-			<h1> raid report </h1>
+			<Paper>
+				<Table>
+			        <TableHead>
+			          <TableRow>
+			            <TableCell>Player</TableCell>
+			            <TableCell >Level</TableCell>
+			            <TableCell>Team</TableCell>
+			          </TableRow>
+			        </TableHead>
+			        <TableBody>
+			              <TableRow key='1'>
+			                <TableCell>Player1</TableCell>
+			                <TableCell >34</TableCell>
+			                <TableCell >Valor</TableCell>
+			              </TableRow>
+			              <TableRow key='2'>
+			                <TableCell>Player2</TableCell>
+			                <TableCell >34</TableCell>
+			                <TableCell >Valor</TableCell>
+			              </TableRow>
+			              <TableRow key='3'>
+			                <TableCell>Player3</TableCell>
+			                <TableCell >34</TableCell>
+			                <TableCell >Valor</TableCell>
+			              </TableRow>
+			              <TableRow key='4'>
+			                <TableCell>Player4</TableCell>
+			                <TableCell >34</TableCell>
+			                <TableCell >Valor</TableCell>
+			              </TableRow>
+			        </TableBody>
+			      </Table>
+		      </Paper>
 			);
 	}		
 }
